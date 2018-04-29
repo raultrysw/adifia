48f8936efb326fa00d7507ee24b66ed4e501ce02
raul <raultrysw@gmail.com>
2018-04-29 17:16:40 +0200

    story aparece solamente cuando el evento pasó y se normalizó el campo location

--- pages/admin/index/events/create.vue
+++ pages/admin/index/events/create.vue
@@ -2,19 +2,21 @@
   <section>
     <h2>Creando un miembro</h2>
     <my-form :errors="errors" :onSubmit="postEvent" text="Guardar evento" urlCb="/events" textCb="Volver">
-      <my-input v-model="event.title" placeholder="Título del evento" type="text" text="Pon el tíulo del evento" />
-      <my-input v-model="event.description" placeholder="Descripción del evento" type="text" text="Pon la descripción del evento" />
-      <rsw-location-picker v-model="location" height="12vh" hint="Busca aquí las coordenadas" />
-      <div>
-        <p>Dia y hora</p>
-        <div>
-          <input v-model="event.time" type="time" />
-          <input v-model="event.date" type="date" />
+      <div class="flex-horizontal form">
+        <my-input v-model="event.title" placeholder="Título del evento" type="text" text="Pon el tíulo del evento" />
+        <my-input v-model="event.description" placeholder="Descripción del evento" type="text" text="Pon la descripción del evento" />
+      </div>
+      
+      <div class="flex-horizontal form">
+        <rsw-location-picker v-model="location" height="25vh" hint="Busca aquí las coordenadas" />
+        <div class="event__timedate">
+          <my-input v-model="event.time" palceholder="Hora del evento" type="time" text="Hora del evento" />
+          <my-input v-model="event.date" palceholder="Dia del evento" type="date" text="Dia del evento" />
         </div>
       </div>
-      <div>
-        <p>¿Como sucedió?</p>
-        <textarea v-model="event.story"></textarea>
+      <div class="event__story">
+        <h3>¿Como sucedió?</h3>
+        <textarea rows="10" v-model="event.story"></textarea>
       </div>
     </my-form>
   </section>
--- pages/admin/index/events/edit.vue
+++ pages/admin/index/events/edit.vue
@@ -5,18 +5,26 @@
         urlCb="/admin/events" textCb="volver atras">
       <my-input type="text" text="Escribe un título"   placeholder="Título del evento" v-model="event.title" />      
       <my-input type="text" text="Escribe una descripción"   placeholder="Descripción del evento" v-model="event.description" />      
-      <my-input type="text" text="Escribe un lugar"   placeholder="Lugar del evento" v-model="event.location" />      
-      <my-input type="text" text="Escribe una hora"   placeholder="Hora del evento" v-model="event.time" />      
-      <my-input type="text" text="Escribe un dia"   placeholder="Dia del evento" v-model="event.date" />      
-      <my-input type="text" text="Escribe una descripción de como sucedio"   placeholder="Sucesos del evento" v-model="event.story" />      
+      <span class="input-control__text">Lugar del evento</span>
+      <rsw-location-picker hint="Lugar del evento" :text="locationParsed.text" :lat="locationParsed.lat" :lng="locationParsed.lng" height="12vh" />
+      <div class="flex-horizontal form">      
+        <my-input type="time" text="Escribe una hora"   placeholder="Hora del evento" v-model="event.time" />      
+        <my-input type="date" text="Escribe un dia"   placeholder="Dia del evento" v-model="event.date" />      
+      </div>
+      <div v-if="consumed" class="event__story">
+        <span class="input-control__text">Como sucedio</span>        
+        <textarea rows="10" v-model="event.story"></textarea>
+      </div>
     </my-form>
   </section>
 </template>
 <script>
 import {mapState, mapMutations} from 'vuex'
 import {putEvent, recoverAllEvents} from '~/api/events'
+import rswLocationPicker from '~/components/rsw-location-picker.vue'
 
 export default {
+  components: {rswLocationPicker},
   data () {
     return {
       errors: []
@@ -27,6 +35,18 @@ export default {
     event () {
       debugger //eslint-disable-line
       return (this.loaded && this.events.filter(event => event._id === this.$route.query.id)[0]) || {}
+    },
+    consumed () {
+      let date = this.event.date
+      let time = this.event.time
+      let d = new Date(date + ' ' + time)
+      let consumed = Date.now() - d.getTime() > 0
+      return consumed
+    },
+    // 38.3459963:-0.4906855000000405:Alicante, España
+    locationParsed () {
+      let [lat, lng, text] = this.event.location.split(':')
+      return {lat, lng, text}
     },
     loaded () {
       return this.events.length > 0
